815e464c92ea8b8ac4cdc4f501fc9c474a6e031c
Tim Henderson <tim.tadh@gmail.com>
2010-07-24 10:49:49 -0400

    dead end work on tst
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- t_tst.py
+++ t_tst.py
@@ -5,29 +5,98 @@ Contact: tim.tadh@hackthology.com
 Copyright (c) 2010 All Rights Reserved.
 Licensed under a BSD style license see the LICENSE file.
 '''
-import unittest
+import unittest, os, base64, itertools, random
 from tst import TST
+
+def strings(s='abcde'):
+	out = list()
+	for i in xrange(len(s)):
+		l = list()
+		for j in xrange(i+1):
+			l.append(s)
+		out += list(''.join(x) for x in itertools.product(*l))
+	return out
 
 class TestTable(unittest.TestCase):
 
 	def test_init(self):
 		t = TST()
 
-	def insert_tester(self, t, tup):
+	def insert_tester1(self, t, tup, s):
+		l = list()
 		for k, v in tup:
 			t[k] = v
-		for k, v in tup:
-			t[k] = v
-		for k, v in tup:
-			self.assertEquals(t[k], v)
-		l = list(tup)
-		l.reverse()
-		for k, v in l:
-			self.assertEquals(t[k], v)
+			l.append((k,v))
+			for k,v in l:
+				try: tv = t[k]
+				except KeyError:
+					print t
+					self.fail('key %s should be in table, %s' % (k, str(k in s)))
+				if tv != v:
+					print t
+				self.assertEquals(tv, v, msg='%s, %s %s' % (k, str(v), str(tv)))
+		#for k, v in tup:
+			#t[k] = v
+		#for k, v in tup:
+			#self.assertEquals(t[k], v)
+		#l = list(tup)
+		#l.reverse()
+		#for k, v in l:
+			#try: tv = t[k]
+			#except KeyError:
+				#print t
+				#self.fail('key %s should be in table, %s' % (k, str(k in s)))
+			#if tv != v:
+				#print t
+				#print 'arg'
+			#self.assertEquals(tv, v, msg='%s, %s %s' % (k, str(v), str(tv)))
+		return True
 
-	def test_insert(self):
+
+	def insert_tester2(self, t, tup, s):
+		for k, v in tup:
+			t[k] = v
+		#for k, v in tup:
+			#t[k] = v
+		#for k, v in tup:
+			#self.assertEquals(t[k], v)
+		#l = list(tup)
+		#l.reverse()
+		for k, v in tup:
+			try: tv = t[k]
+			except KeyError:
+				print t
+				self.fail('key %s should be in table, %s' % (k, str(k in s)))
+			if tv != v:
+				print t
+				print 'arg'
+			self.assertEquals(tv, v, msg='%s, %s %s' % (k, str(v), str(tv)))
+		return True
+
+	#def test_insert(self):
+		#t = TST()
+		#self.insert_tester(t, (('b', 2), ('a', 1), ('d', 4), ('daf', 5), ('db', 6), ('dac', 3)))
+
+	#def test_random(self):
+		#for j in xrange(500):
+			#s = set()
+			#l = list()
+			#for x in xrange(150):
+				#k = base64.b64encode(os.urandom(x%10 + 1)).rstrip('=')
+				#if k in s: continue
+				#l.append((k, x))
+				#s.add(k)
+			#t = TST()
+			#if not self.insert_tester1(t, l, s): break
+
+	def test_complete(self):
+		#random.seed(os.urandom(300))
+		s = strings('abc')
+		l = [(s,i) for i,s in enumerate(s)]
+		random.shuffle(l)
 		t = TST()
-		self.insert_tester(t, (('b', 2), ('a', 1), ('d', 4), ('daf', 5), ('db', 6), ('dac', 3)))
+		self.insert_tester1(t, l, set(s))
+		#print t
 
 
 if __name__ == '__main__':
--- tst.py
+++ tst.py
@@ -7,6 +7,7 @@ Licensed under a BSD style license see the LICENSE file.
 '''
 from collections import deque
 from os.path import sep as PATH_SEP
+import sys
 
 END = '\x00'
 NOITEM = type('NOITEM', (object,), {'__str__':lambda self: 'NOITEM', '__repr__':lambda self: 'NOITEM'})()
@@ -35,30 +36,34 @@ class TST(object):
 			ch = symbol[d]
 			#print d, n, len(symbol), ch, n.key[d]
 			if d+1 == len(symbol) and d+1 == len(n.key):
-				#print 'case1'
+				sys.stderr.write('case1\n')
+				sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
 				if   ch <  n.ch: n.l = insert(n.l, d)
 				elif ch == n.ch:
 					if n.key == symbol: n.val = obj
-					else: raise Exception, "same object? no ..."
+					else:
+						print self
+						raise Exception, "same object? no ..."
 				elif ch >  n.ch: n.r = insert(n.r, d)
 				return n
 			elif d+1 == len(n.key):
-				#print 'case2'
+				sys.stderr.write('case2\n')
+				sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
 				if   ch <  n.ch: n.l = insert(n.l, d)
 				elif ch == n.ch: n.m = insert(n.m, d+1)
 				elif ch >  n.ch: n.r = insert(n.r, d)
 				return n
 			elif d+1 == len(symbol):
-				#print 'case3'
-				#print symbol, n.key, n.ch, ch
+				sys.stderr.write('case3\n')
+				sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
 				new = node(ch, key=symbol, val=obj)
 				if ch <  n.ch: new.r = n
 				if ch == n.ch: new.m = n
 				if ch >  n.ch: new.l = n
 				return new
 			else:
-				#print 'case4'
-				#print n.key, symbol, n.ch, ch
+				sys.stderr.write('case4\n')
+				sys.stderr.write(str((symbol, n.key, n.ch, ch, ch<n.ch, ch==n.ch, ch>n.ch))+'\n')
 				new = node(n.ch, m=node(n.key[d+1], key=n.key, val=n.val))
 				if   ch <  n.key[d]: new.l = node(ch, key=symbol, val=obj)
 				elif ch == n.key[d]: new.m = split(d+1, new.m)
@@ -66,27 +71,51 @@ class TST(object):
 				return new
 			raise Exception
 		def insert(n, d):
-			#print n, d
+			sys.stderr.write('-->' + str(n) + ' ' + str(d) + '\n')
+			if d >= len(symbol):
+				sys.stderr.write(str((d, len(symbol), symbol,n))+'\n')
+				print self
 			ch = symbol[d]
 			if n == None:
+				sys.stderr.write('case0\n')
+				#sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
 				return node(ch, key=symbol, val=obj)
 			if not n.internal():
 				return split(d, n)
-			if n.accepting and d+1 == len(symbol) and d+1 == len(n.key) and ch == n.ch:
-				if n.key == symbol:
+			if d+1 == len(symbol) and ch == n.ch:
+				if n.accepting and n.key == symbol:
+					sys.stderr.write('case5\n')
+					sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
 					n.val = obj
 					return n
+				elif n.accepting:
+					sys.stderr.write('case6\n')
+					sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
+				else:
+					sys.stderr.write('case7\n')
+					sys.stderr.write(str((symbol, n.key, n.ch, ch))+'\n')
+					#new = node(ch, key=symbol, val=obj)
+					n.key = symbol
+					n.val = obj
+					n.accepting = True
+					#if ch <  n.ch: new.r = n
+					#if ch == n.ch: new.m = n
+					#if ch >  n.ch: new.l = n
+					return n
+			sys.stderr.write('case8\n')
+			sys.stderr.write(str((symbol, n.key, n.ch, ch, ch<n.ch, ch==n.ch, ch>n.ch))+'\n')
 			if   ch <  n.ch: n.l = insert(n.l, d)
 			elif ch == n.ch: n.m = insert(n.m, d+1)
 			elif ch >  n.ch: n.r = insert(n.r, d)
 			return n
 		#print
 		#print
-		#print 'insert', symbol, obj
+		sys.stderr.write('\n\n\ninsert ' + symbol + ' ' + str(obj)+'\n')
 		self.root = insert(self.root, 0)
 
 	def __getitem__(self, symbol):
 		def get(n, d):
+			sys.stderr.write(str(n)+'\n')
 			if n == None:
 				raise KeyError, "Symbol '%s' is not in table." % symbol
 			#print d, len(symbol)
@@ -101,6 +130,7 @@ class TST(object):
 			if n.key == symbol:
 				return n.val
 			raise KeyError, "Symbol '%s' is not in table." % symbol
+		sys.stderr.write('\n\n\nget ' + symbol +'\n')
 		return get(self.root, 0)
 
 	def __delitem__(self, symbol):
@@ -113,7 +143,43 @@ class TST(object):
 		pass
 
 	def __str__(self):
-		return "{" + str(self.root) + "}"
+		q = deque()
+		q.append((self.root, 0))
+		i = 0
+		nodes = dict()
+		edges = dict()
+		while len(q) > 0:
+			n, m = q.pop()
+			if not n: continue
+			q.append((n.r, 0))
+			q.append((n.m, 1))
+			q.append((n.l, 2))
+			if n not in nodes:
+				#sys.stderr.write(str(m) + ' ' + str(n.accepting) + '\n')
+				nodes[n] = ('node' + str(i), m, n.accepting)
+				i += 1
+			if n not in edges:
+				edges[n] = list()
+			if n.l != None: edges[n].append(n.l)
+			if n.m != None: edges[n].append(n.m)
+			if n.r != None: edges[n].append(n.r)
+		s = 'digraph tst {\n'
+		for k,v in nodes.iteritems():
+			name, shape, accept = v
+			style = ''
+			if accept: style = 'style="filled" fillcolor="#63ADD0"'
+			if shape == 1: n = '%s[shape="box" %s label="%s"];\n' % (name, style, str(k))
+			elif shape == 2: n = '%s[shape="diamond" %s label="%s"];\n' % (name, style, str(k))
+			else: n = '%s[%s label="%s"];\n' % (name, style, str(k))
+			s += n
+			#sys.stderr.write(n)
+		for k,v in edges.iteritems():
+			n1, a, b = nodes[k]
+			for e in v:
+				n2, a, b = nodes[e]
+				s += '%s -> %s;\n' % (n1, n2)
+		s += '}\n'
+		return s
 
 	def __repr__(self):
 		return str(self)
@@ -142,8 +208,18 @@ class node(object):
 	def internal(self):
 		return self.l != None or self.m != None or self.r != None
 
+	def __hash__(self):
+		return hash((self.ch, self.key, self.val, self.l, self.m, self.r, self.accepting))
+
+	def __eq__(self, a):
+		return hash(self) == hash(a)
+
+	def __ne__(self, a):
+		return hash(self) != hash(a)
+
 	def __str__(self):
-		return str((self.ch, self.key, self.val, self.accepting, [self.l, self.m, self.r]))
+		if self.accepting: return "%s %s %s" % (self.ch, self.key, str(self.val))
+		return self.ch
 
 	def __repr__(self):
 		return str(self)
