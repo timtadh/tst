f47ccb34b9a4096007326292e136dbfa3d7b6b69
Tim Henderson <tim.tadh@gmail.com>
2011-05-19 14:31:39 -0400

    setup script
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='tst',
+      version='.8',
+      description=(
+        'A ternary search trie.'
+      ),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/tst',
+      license='BSD',
+      packages=['tst'],
+)
